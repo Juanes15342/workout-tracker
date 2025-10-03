915d85a72e4c1fb537bd7cf52d9875c90cf2a567
Juan <juanes15342@gmail.com>
2025-10-02 23:08:39 -0500

    implementación de rutas GET (lista e individual).

--- src/controllers/exercises.controller.js
+++ src/controllers/exercises.controller.js
@@ -1,6 +1,12 @@
-exports.getExercises = (req, res) => res.json({ message: "GET all exercises scaffold" });
-exports.getExerciseById = (req, res) => res.json({ message: "GET one exercise scaffold" });
-exports.createExercise = (req, res) => res.json({ message: "POST exercise scaffold" });
-exports.updateExercise = (req, res) => res.json({ message: "PUT exercise scaffold" });
-exports.patchExercise = (req, res) => res.json({ message: "PATCH exercise scaffold" });
-exports.deleteExercise = (req, res) => res.json({ message: "DELETE exercise scaffold" });
+const exercises = require("../data/exercises.data");
+
+exports.getExercises = (req, res) => {
+  res.json(exercises);
+};
+
+exports.getExerciseById = (req, res) => {
+  const { id } = req.params;
+  const exercise = exercises.find(e => e.id === parseInt(id));
+  if (!exercise) return res.status(404).json({ error: "Ejercicio no encontrado" });
+  res.json(exercise);
+};
