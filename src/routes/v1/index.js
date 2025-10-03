3ddeb61ce05f3f09c875cab1976d11c3245fe75b
Juan <juanes15342@gmail.com>
2025-10-02 23:52:15 -0500

    Fix conflictos en merge

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,19 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-<<<<<<< HEAD
 const userRoutes = require("./user.routes");
 const workoutRoutes = require("./workouts.routes");
-=======
 const usersRoutes = require('./progress.routes');
->>>>>>> feat/progress
 
 router.use("/users", userRoutes);
 router.use("/workouts", workoutRoutes);
 
-<<<<<<< HEAD
+ 
 module.exports = router;
-=======
+
 const createProgress = (newProgress) => {
   const progressItem = { id: progress.length + 1, ...newProgress };
   progress.push(progressItem);
@@ -21,5 +18,4 @@ const createProgress = (newProgress) => {
 };
 
 module.exports = { progress, createProgress };
-
->>>>>>> feat/progress
+ feat/progress
